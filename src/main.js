690c9f5a3fd36935738fe39fe932ba4f42b665ee
luozhixian <1597395958@qq.com>
2020-06-15 13:55:36 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './styles/index.scss'
 import 'amfe-flexible/index.js'
 import * as filters from './filters/index'
 Object.keys(filters).forEach((key) => {
