aade93bfeb0747685d0fed1f6b8d8c72c7be75cd
shine-lzx <1597395958@qq.com>
2020-06-23 14:21:58 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import VideoPlayer from 'vue-video-player'
-import 'vue-video-player/src/custom-theme.css'
-import 'video.js/dist/video-js.css'
-Vue.use(VideoPlayer)
+import VideoPlayer from "vue-video-player";
+import "vue-video-player/src/custom-theme.css";
+import "video.js/dist/video-js.css";
+import "amfe-flexible/index.js";
+import 'normalize.css/normalize.css'
+Vue.use(VideoPlayer);
 Vue.config.productionTip = false;
 new Vue({
   router,
